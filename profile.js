befc96f6a9456d366e49a14cb403d097508f29e0
LuisGustavo915 <143193687+LuisGustavo915@users.noreply.github.com>
2023-11-23 21:41:22 -0400

    Commit

---
+++ profile.js
@@ -0,0 +1,111 @@
+export const skills =[
+    {
+        skill: 'JavaScript',
+        percentage: 100,
+    },
+    {
+        skill: 'Golang',
+        percentage: 80,
+    },
+    {
+        skill: 'Node js',
+        percentage: 90,
+    },
+    {
+        skill: 'Blockchain',
+        percentage: 40,
+    },
+    {
+        skill: 'Physics',
+        percentage: 70,
+    },
+    {
+        skill: 'Enconomics',
+        percentage: 50,
+    },
+    {
+        skill: 'Machine Learnig',
+        percentage: 70,
+    },
+]
+export const experiences =[
+    {
+        title: "Software developer at Microsoft",
+        description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc",
+         
+        from: 2000,
+        to: 2005,
+      },
+      {
+        title: "Software developer at Google",
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        from: 2005,
+        to: 2007,
+      },
+      {
+        title: "Software developer at SpaceX",
+        description:
+          "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem officiis fugiat vel animi aliquam inventore rem. Quo laudantium temporibus cupiditate. Aut?",
+        from: 2010,
+      },
+    ];
+export const proyects =[
+    {
+        Nome: "Awesome website 1",
+        description: "Lorem ipsum dolor sit amet incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+        image: "portfolio1.jpeg"
+
+    },
+    {
+        Nome: "Awesome website 1",
+        description: "Lorem ipsum dolor sit amet incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+        image: "portfolio2.jpg"
+
+    },
+    {
+        Nome: "Awesome website 1",
+        description: "Lorem ipsum dolor sit amet incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+        image: "portfolio3.png"
+
+    },
+    {
+        Nome: "Awesome website 1",
+        description: "Lorem ipsum dolor sit amet incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+        image: "portfolio4.png"
+
+    },
+    {
+        Nome: "Awesome website 1",
+        description: "Lorem ipsum dolor sit amet incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+        image: "portfolio5.jpeg"
+
+    },
+    {
+        Nome: "Awesome website 1",
+        description: "Lorem ipsum dolor sit amet incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+        image: "portfolio6.jpeg"
+
+    },
+];
+export const posts =[
+{
+    title: "React",
+    content: "Lorem ipsum dolor sit amet incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+    imageURL:"https://assets.toptal.io/images?url=https%3A%2F%2Fbs-uploads.toptal.io%2Fblackfish-uploads%2Fcomponents%2Fblog_post_page%2Fcontent%2Fcover_image_file%2Fcover_image%2F1282565%2Fregular_1708x683_cover-react-context-api-4929b3703a1a7082d99b53eb1bbfc31f.png",
+
+},
+{
+    title: "Vue",
+    content: "Lorem ipsum dolor sit amet incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+    imageURL:"https://picx.zhimg.com/v2-ff097f2774f136ba18afd39d1413e699_720w.jpg?source=172ae18b",
+    
+},
+{
+    title: "Angular",
+    content: "Lorem ipsum dolor sit amet incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+    imageURL:"https://assets.toptal.io/images?url=https%3A%2F%2Fbs-uploads.toptal.io%2Fblackfish-uploads%2Fcomponents%2Fblog_post_page%2Fcontent%2Fcover_image_file%2Fcover_image%2F1131363%2Fregular_1708x683_cover-angular-5-tutorial-325403e130ba3b2c367174b73bb7275a.png",
+    
+},
+
+];
